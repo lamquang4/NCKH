e1222e9f0940b0dc2fe5c2922d0454134d266da0
Quang lam <lamdieuquang0105@gmail.com>
2025-12-23 18:21:31 +0700

    cập nhật frontend

--- frontend/src/components/chatbox/ChatFooter/ChatInput.tsx
+++ frontend/src/components/chatbox/ChatFooter/ChatInput.tsx
@@ -11,8 +11,9 @@ const ChatInput = forwardRef<HTMLTextAreaElement, Props>(({ onInput }, ref) => {
       rows={1}
       onInput={onInput}
       name="message"
+      id="message"
       placeholder="Nhập nội dung..."
-      className="flex-1 w-full resize-none overflow-hidden py-1 border-0 text-[0.9rem] outline-0 bg-transparent"
+      className="px-3 py-3 flex-1 w-full resize-none overflow-hidden border-0 text-[0.9rem] outline-0 bg-transparent"
     />
   );
 });
