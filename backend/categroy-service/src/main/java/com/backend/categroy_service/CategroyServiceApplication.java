672a53d76aff6563c2b2c86ccb2d1ed203ca02c4
Quang lam <lamdieuquang0105@gmail.com>
2026-01-11 17:16:56 +0700

    backend

---
+++ backend/categroy-service/src/main/java/com/backend/categroy_service/CategroyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.categroy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategroyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategroyServiceApplication.class, args);
+	}
+
+}
---
+++ backend/categroy-service/src/test/java/com/backend/categroy_service/CategroyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.categroy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategroyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
