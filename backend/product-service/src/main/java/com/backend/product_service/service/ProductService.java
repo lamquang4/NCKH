a7e1c64c0d8a59f5970b0efca30447d5a7c78d37
Quang lam <lamdieuquang0105@gmail.com>
2026-01-26 09:48:42 +0700

    sửa lỗi

--- backend/product-service/src/main/java/com/backend/product_service/service/ProductService.java
+++ backend/product-service/src/main/java/com/backend/product_service/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.backend.product_service.service;
 
+import org.springframework.stereotype.Service;
+
 import com.backend.product_service.repository.ProductRepository;
 
+@Service
 public class ProductService {
     private final ProductRepository productRepository;
 
