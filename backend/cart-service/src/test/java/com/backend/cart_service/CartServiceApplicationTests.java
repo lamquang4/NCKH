8724f9a4c72b7aad1693309afebf0cd374b8be67
Quang lam <lamdieuquang0105@gmail.com>
2026-01-30 23:33:06 +0700

    sửa lỗi env #1

--- backend/cart-service/src/test/java/com/backend/cart_service/CartServiceApplicationTests.java
+++ backend/cart-service/src/test/java/com/backend/cart_service/CartServiceApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CartServiceApplicationTests {
 
-    @MockBean
-    RedisTemplate<String, Object> redisTemplate;
+	@MockBean
+	RedisTemplate<String, Object> redisTemplate;
 
 	@Test
 	void contextLoads() {
