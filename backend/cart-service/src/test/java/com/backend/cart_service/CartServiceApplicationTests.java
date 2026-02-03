7744036f71448c147184b514387dca08b179d24d
Quang lam <lamdieuquang0105@gmail.com>
2026-02-03 12:52:39 +0700

    sửa lỗi ci/cd ở api-gateway

--- backend/cart-service/src/test/java/com/backend/cart_service/CartServiceApplicationTests.java
+++ backend/cart-service/src/test/java/com/backend/cart_service/CartServiceApplicationTests.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
 		"spring.autoconfigure.exclude=" +
 				"org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
 				"org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
 })
-@ActiveProfiles("test")
 class CartServiceApplicationTests {
 
 	@MockBean
